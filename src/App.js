25d2fa0bbd9ed044bc9b703ff38fd55f43a0369b
VanHau87 <nguyenvanh29@gmail.com>
2022-05-28 21:51:25 +0700

    Set up project and create info box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import "./App.css";
+import InfoBox from "./InfoBox";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  const handleChange = (e) => setCountry(e.target.value);
+  useEffect(() => {
+    //async => send a request, wait for it, do something
+    const fetchCountries = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((res) => res.json())
+        .then((res) => res.map(({ country }) => country))
+        .then((res) => setCountries(res));
+    };
+    fetchCountries();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className="app_dropdown" variant="outlined">
+          <Select value={country} variant="outlined" onChange={handleChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country, idx) => (
+              <MenuItem value={country} key={idx}>
+                {country}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div className="app__stats">
+        <InfoBox title="Coronavirus Cases" />
+        <InfoBox title="Recovered" />
+        <InfoBox title="Deaths" />
+      </div>
+      {/* Table */}
+      {/* Graph */}
+      {/* Map */}
     </div>
   );
 }
